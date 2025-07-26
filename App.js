82a705f76d127c92696a1bf9557f88e2cb898447
Hasan Alasker <hasanalasker2004@gmail.com>
2025-07-26 19:16:51 +0300

    first push

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Home from './pages/Home'
+
+const Stack = createNativeStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator
+    initialRouteName="Home"
+    screenOptions={{ headerShown: false, animation: "none" }}
+  >
+    <Stack.Screen name="Home" component={Home}></Stack.Screen>
+  </Stack.Navigator>
+);
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StackNavigator></StackNavigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
 });

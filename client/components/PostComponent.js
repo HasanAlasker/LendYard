4f7c82b8efcce0dab28f2a2e8d8f696d1519479c
Hasan Alasker <hasanalasker2004@gmail.com>
2025-07-28 13:46:55 +0300

    d

---
+++ client/components/PostComponent.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import useThemedStyles from "../hooks/useThemedStyles";
+
+function PostComponent({ children, style }) {
+  const styles = useThemedStyles(getStyles);
+
+  return <View style={[styles.post, style]}>{children}</View>;
+}
+const getStyles = (theme) =>
+  StyleSheet.create({
+    post: {
+      width: "90%",
+      paddingHorizontal: 20,
+      paddingTop: 30,
+      paddingBottom: 20,
+      borderRadius: 15,
+
+      backgroundColor: theme.post,
+      marginHorizontal: "auto",
+      rowGap: 20,
+      shadowColor: "black",
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowOpacity: 0.27,
+      shadowRadius: 4.65,
+
+      elevation: 6,
+    },
+  });
+export default PostComponent;
---
+++ client/components/TopNav.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import useThemedStyles from "../hooks/useThemedStyles";
+import AppText from "../config/AppText";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+function TopNav(props) {
+  const styles = useThemedStyles(getStyles);
+  const navigate = useNavigation();
+  const route = useRoute();
+
+  const navigateAndMakeActive = () =>{
+    
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <TouchableOpacity onPress={navigateAndMakeActive}>
+          <AppText style={styles.text}>
+            {route.name === "Book" ? "Given" : "Sent"}
+          </AppText>
+        </TouchableOpacity>
+        <View style={styles.line}></View>
+        <TouchableOpacity onPress={navigateAndMakeActive}>
+          <AppText style={styles.text}>
+            {route.name === "Book" ? "Taken" : "Got"}
+          </AppText>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const getStyles = (theme) =>
+  StyleSheet.create({
+    container: {
+      width: "100%",
+      paddingTop: 20,
+      borderBottomRightRadius: 30,
+      borderBottomLeftRadius: 30,
+      zIndex: 100,
+      paddingBottom: 25,
+      backgroundColor: theme.backgroundColor,
+    },
+    row: {
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+      gap: 20,
+    },
+    text: {
+      fontSize: 20,
+      color: theme.sec_text,
+    },
+    line: {
+      backgroundColor: theme.sec_text,
+      height: 25,
+      width: 2,
+    },
+  });
+
+export default TopNav;
